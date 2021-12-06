b522a865f57d14968d608e54f381482e3dbdc96c
potatozhb <zhaohongbo1980@gmail.com>
2021-12-06 00:16:24 -0600

    3D maps

--- Test/MainWindow.xaml.cs
+++ Test/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UniversalMap;
 
 namespace Test
 {
@@ -30,6 +31,8 @@ namespace Test
         private void Add_Click(object sender, RoutedEventArgs e)
         {
             model.MyContent++;
+
+            
         }
     }
 }
